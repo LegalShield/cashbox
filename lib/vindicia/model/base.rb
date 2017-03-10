9b8e3dca5afa31770c7767b999c13653d0a46cdb
Jonathon Storer <JonathonStorer@legalshieldcorp.com>
2017-03-09 23:00:53 -0600

    wi

--- lib/vindicia/model/base.rb
+++ lib/vindicia/model/base.rb
@@ -1,11 +1,9 @@
 module Vindicia::Model
   class Base < Hashie::Trash
-
     include Hashie::Extensions::MergeInitializer
     include Hashie::Extensions::IgnoreUndeclared
     include Hashie::Extensions::IndifferentAccess
     include Hashie::Extensions::Dash::Coercion
-    #include Hashie::Extensions::MethodAccess
 
     def object
       self.class.name.split('::').last
