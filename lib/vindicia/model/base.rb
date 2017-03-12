11ec2f145f65c6a5ea3baac6d6af6c368862f323
Jonathon Storer <JonathonStorer@legalshieldcorp.com>
2017-03-12 17:47:06 -0400

    wip

--- lib/vindicia/model/base.rb
+++ lib/vindicia/model/base.rb
@@ -8,5 +8,9 @@ module Vindicia::Model
     def object
       self.class.name.split('::').last
     end
+
+    #def to_json(*options)
+      #as_json(*options).to_json(*options)
+    #end
   end
 end
