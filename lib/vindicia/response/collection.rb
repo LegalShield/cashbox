5afe839b9ecdc4c8c1bf6ab5e90b128d31692a5e
Jonathon Storer <JonathonStorer@legalshieldcorp.com>
2017-03-22 08:16:38 -0700

    I can fetch as many or few records as needed
    closes #13

--- lib/vindicia/response/collection.rb
+++ lib/vindicia/response/collection.rb
@@ -1,13 +1,9 @@
 module Vindicia::Response
-  class Collection < Base
+  class Collection < Object
     extend Forwardable
     include Enumerable
 
     def_delegators :body, :each
     undef_method :min, :max, :sort
-
-    def total_count
-      raw_body['total_count']
-    end
   end
 end
--- spec/unit/response/collection_spec.rb
+++ spec/unit/response/collection_spec.rb
@@ -1,30 +1,9 @@
 require 'spec_helper'
 
 describe Vindicia::Response::Collection do
-  subject { Vindicia::Response::Collection.new(nil) }
+  subject { Vindicia::Response::Collection.new('body') }
 
-  it { is_expected.to be_a(Vindicia::Response::Base) }
-end
-
-describe 'instance methods' do
-  let(:http_response) do
-    double('http_response', {
-      to_h: {
-        'object' => 'List',
-        'data' => [ { 'object' => 'Base' } ],
-        'total_count' => 3847
-      }
-    })
-  end
-
-  let(:request) { double('request', { perform: http_response }) }
-  subject { Vindicia::Response::Collection.new(request) }
-
-  context '#total_count' do
-    it 'returns the total count from the response' do
-      expect(subject.total_count).to eql(3847)
-    end
-  end
+  it { is_expected.to be_a(Vindicia::Response::Object) }
 
   it { is_expected.to delegate_method(:each).to(:body) }
   it { is_expected.to be_an(Enumerable) }
