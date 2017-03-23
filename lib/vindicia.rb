bc311144bca6cb1a977c0dfea1810f7a6a92e432
Jonathon Storer <JonathonStorer@legalshieldcorp.com>
2017-03-23 09:39:52 -0700

    wip

--- lib/vindicia.rb
+++ lib/vindicia.rb
@@ -67,14 +67,4 @@ module Vindicia
   module Concern
     autoload :Objectable, 'vindicia/concern/objectable'
   end
-
-  #module Vindicia::Repository
-    #autoload :Base,          'vindicia/repository/base'
-    #autoload :Account,       'vindicia/repository/account'
-    #autoload :BillingPlan,   'vindicia/repository/billing_plan'
-    #autoload :PaymentMethod, 'vindicia/repository/payment_method'
-    #autoload :Product,       'vindicia/repository/product'
-    #autoload :Subscription,  'vindicia/repository/subscription'
-    #autoload :Transaction,   'vindicia/repository/transaction'
-  #end
 end
