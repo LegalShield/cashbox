509afb319a18de510ee2f907d54d85b6b3ee7d4d
Jonathon Storer <JonathonStorer@legalshieldcorp.com>
2017-03-28 10:25:26 -0400

    wip

--- spec/integration/subscription_spec.rb
+++ spec/integration/subscription_spec.rb
@@ -19,11 +19,6 @@ describe 'Subscription' do
         })
     end
 
-    it do
-      binding.pry
-      puts 1
-    end
-
     it { is_expected.to be_a(Vindicia::Subscription) }
 
     its(:object)         { is_expected.to eql('Subscription') }
@@ -33,28 +28,5 @@ describe 'Subscription' do
     its(:account)        { is_expected.to be_a(Vindicia::Account)  }
     its(:billing_plan)   { is_expected.to be_a(Vindicia::BillingPlan) }
     its(:payment_method) { is_expected.to be_a(Vindicia::PaymentMethod) }
-
-    #its(:billing_descriptor)               { is_expected.to eql('1') }
-
-    #its(:amount)                           { is_expected.to eql(10.71) }
-    #its(:billing_attempt)                  { is_expected.to eql(0) }
-    #its(:currency)                         { is_expected.to eql('USD') }
-    #its(:items)                            { is_expected.to be_an(Array) }
-    #its(:original_billing_date)            { is_expected.to eql(DateTime.parse('2017-02-11T00:00:00-08:00')) }
-    #its(:payment_processor)                { is_expected.to eql('PayPal') }
-    #its(:payment_processor_transaction_id) { is_expected.to eql("bbbbbbbbbbbbbbbb") }
-    #its(:status_log)                       { is_expected.to be_an(Array) }
-    #its(:subscription)                     { is_expected.to be_a(Vindicia::Subscription)  }
-    #its(:subscription_sequence)            { is_expected.to eql(2) }
-    #its(:to_be_captured)                   { is_expected.to eql(nil) }
-
-    #it 'has items as Vindicia::TransactionItem' do
-      #expect(subject.items.first).to be_a(Vindicia::TransactionItem)
-    #end
-
-    #it 'has status log as Vindicia::TransactionStatus' do
-      #expect(subject.status_log.first).to be_a(Vindicia::TransactionStatus)
-    #end
   end
 end
-
