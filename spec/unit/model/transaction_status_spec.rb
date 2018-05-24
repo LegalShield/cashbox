cbb778a376fb807e5ff4743047c55d0f3a34cef1
Daniel Long <danlong5@gmail.com>
2018-05-24 15:29:54 -0500

    more tests

--- spec/unit/model/transaction_status_spec.rb
+++ spec/unit/model/transaction_status_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Cashbox::TransactionStatus do
   it { is_expected.to be_a(Cashbox::Model) }
+  it { is_expected.to be_a(Cashbox::Concern::Objectable) }
 
   it { is_expected.to have_property(:created).coercing_with(Cashbox::Type.DateTime) }
   it { is_expected.to have_property(:payment_method_type) }
