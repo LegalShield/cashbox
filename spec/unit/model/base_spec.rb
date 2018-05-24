12c018b41b4229febd4963874bb219e7a86878e9
Daniel Long <danlong5@gmail.com>
2018-05-24 14:33:36 -0500

    working fleshing out unit tests

--- spec/unit/model/base_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe Cashbox::Model do
-  it { is_expected.to be_a(Hashie::Trash) }
-  it { is_expected.to be_a(Hashie::Extensions::MergeInitializer) }
-  it { is_expected.to be_a(Hashie::Extensions::IgnoreUndeclared) }
-  it { is_expected.to be_a(Hashie::Extensions::IndifferentAccess) }
-  it { is_expected.to be_a(Hashie::Extensions::Dash::Coercion) }
-end
