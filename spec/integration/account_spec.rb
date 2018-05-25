4b6f109ccbc7acd85d411822b57c2e6df46ebe9b
Daniel Long <danlong5@gmail.com>
2018-05-25 10:20:31 -0500

    wip

--- spec/integration/account_spec.rb
+++ spec/integration/account_spec.rb
@@ -74,4 +74,67 @@ describe 'Account' do
       expect(stub).to have_been_requested
     end
   end
+
+  describe 'searching for an account' do
+    let(:json) do
+      json = JSON.parse(fixture('account'))
+      json
+    end
+
+    let!(:stub_find) do
+      stub_get("/accounts/#{json['vid']}")
+        .to_return({
+          :status => 200,
+          :body => fixture('account'),
+          :headers => { 'Content-Type': 'application/json' }
+      })
+    end
+
+    let!(:stub_where) do
+      stub_get("/accounts?limit=100&vid=#{json['vid']}")
+        .to_return({
+          :status => 200,
+          :body => fixture('account'),
+          :headers => { 'Content-Type': 'application/json' }
+      })
+    end
+
+    let!(:stub_first) do
+      stub_get("/accounts?limit=1")
+        .to_return({
+          :status => 200,
+          :body => fixture('account'),
+          :headers => { 'Content-Type': 'application/json' }
+      })
+    end
+
+    let!(:stub_all) do
+      stub_get("/accounts?limit=100")
+        .to_return({
+          :status => 200,
+          :body => fixture('account'),
+          :headers => { 'Content-Type': 'application/json' }
+      })
+    end
+
+    it 'makes the correct call to find' do
+      Cashbox::Account.find(json['vid'])
+      expect(stub_find).to have_been_requested
+    end
+
+    it 'makes the correct call to where' do
+      Cashbox::Account.where(vid: json['vid'])
+      expect(stub_where).to have_been_requested
+    end
+
+    it 'makes the correct call to first' do
+      Cashbox::Account.first
+      expect(stub_first).to have_been_requested
+    end
+
+    it 'makes the correct call to all' do
+      Cashbox::Account.all
+      expect(stub_all).to have_been_requested
+    end
+  end
 end
