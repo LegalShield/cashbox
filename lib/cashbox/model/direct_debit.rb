92286044e70b40979625adf11ea2f4b570c3e02b
Daniel Long <danlong5@gmail.com>
2018-05-25 13:24:18 -0500

    moving this back

--- lib/cashbox/model/direct_debit.rb
+++ lib/cashbox/model/direct_debit.rb
@@ -2,12 +2,12 @@ module Cashbox
   class DirectDebit < Model
     include Concern::Objectable
 
+    property :vid
     property :account
     property :account_length
     property :bank_sort_code
     property :country_code
     property :last_digits
     property :rib_code
-    property :vid
   end
 end
