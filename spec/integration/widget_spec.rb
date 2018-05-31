f924bea127b9b44cc7636651ab5bd52d5623509a
Jonathon Storer <jonathon.scott.storer@gmail.com>
2018-05-30 22:09:08 -0400

    wip

--- spec/integration/widget_spec.rb
+++ spec/integration/widget_spec.rb
@@ -8,6 +8,10 @@ describe 'Widget' do
     property :name
   end
 
+  def api_response(body)
+    { :status => 200, :body => body, :headers => headers }
+  end
+
   let(:headers) { { 'Content-Type': 'application/json' } }
 
   describe '#save' do
@@ -18,7 +22,7 @@ describe 'Widget' do
       let!(:stub) do
         stub_post('/widgets')
           .with({ body: { id: 1, name: 'my-widget' }.to_json })
-          .to_return({ :status => 200, :body => body, :headers => headers })
+          .to_return(api_response(body))
       end
 
       before do
@@ -39,7 +43,7 @@ describe 'Widget' do
       let!(:stub) do
         stub_post('/widgets/1')
           .with({ body: { vid: 1, id: 1, name: 'my-widget' }.to_json })
-          .to_return({ :status => 200, :body => body, :headers => headers })
+          .to_return(api_response(body))
       end
 
       before do
@@ -57,7 +61,7 @@ describe 'Widget' do
     let(:body) { { 'object' => 'Widget', 'vid' => 1, 'id' => 1, 'name' => 'my-widget' }.to_json }
 
     let!(:stub) do
-      stub_get('/widgets/1').to_return({ :status => 200, :body => body, :headers => headers })
+      stub_get('/widgets/1').to_return(api_response(body))
     end
 
     before do
@@ -78,7 +82,7 @@ describe 'Widget' do
       it 'makes the correct request' do
         stub = stub_get('/widgets')
           .with({ query: { name: 'my-widget', limit: 100 } })
-          .to_return({ :status => 200, :body => body, :headers => headers })
+          .to_return(api_response(body))
 
         subject.where({ name: 'my-widget' })
 
@@ -92,7 +96,7 @@ describe 'Widget' do
       it 'makes the correct request' do
         stub = stub_get('/widgets')
           .with({ query: { name: 'my-widget', limit: 10 } })
-          .to_return({ :status => 200, :body => body, :headers => headers })
+          .to_return(api_response(body))
 
         subject.where({ name: 'my-widget', limit: 10 })
 
@@ -118,17 +122,81 @@ describe 'Widget' do
       it 'makes the correct request' do
         first_stub = stub_get('/widgets')
           .with({ query: { name: 'my-widget', limit: 100 } })
-          .to_return({ :status => 200, :body => first_body, :headers => headers })
+          .to_return(api_response(first_body))
 
         second_stub = stub_get('/widgets')
           .with({ query: { name: 'my-widget', limit: 100, starting_after: 1 } })
-          .to_return({ :status => 200, :body => second_body, :headers => headers })
+          .to_return(api_response(second_body))
 
         subject.where({ name: 'my-widget', limit: 120 })
 
         expect(first_stub).to have_been_requested
         expect(second_stub).to have_been_requested
       end
+    end
+  end
+
+  describe '.all' do
+    subject { self.class::Widget }
+
+    let(:first_body) { {
+      'object' => 'List',
+      'data' => [ { 'object' => 'Widget', 'vid' => 1, 'id' => 1, 'name' => 'my-widget' } ],
+      'next' => '/widgets?limit=100&starting_after=1',
+      'previous' => '/widgets?limit=100&ending_before=1'
+    }.to_json }
+
+    let(:second_body) { {
+      'object' => 'List',
+      'data' => [ { 'object' => 'Widget', 'vid' => 2, 'id' => 2, 'name' => 'my-widget' } ],
+      'next' => '/widgets?limit=100&starting_after=2',
+      'previous' => '/widgets?limit=100&ending_before=2'
+    }.to_json }
+
+    let(:third_body) { {
+      'object' => 'List',
+      'data' => [ { 'object' => 'Widget', 'vid' => 3, 'id' => 3, 'name' => 'my-widget' } ],
+      'next' => nil,
+      'previous' => '/widgets?limit=100&ending_before=3'
+    }.to_json }
+
+    it 'makes requests until there are no more records' do
+      first_stub  = stub_get('/widgets')
+        .with({ query: { limit: 100 } })
+        .to_return(api_response(first_body))
+      second_stub = stub_get('/widgets')
+        .with({ query: { limit: 100, starting_after: 1 } })
+        .to_return(api_response(second_body))
+      third_stub  = stub_get('/widgets')
+        .with({ query: { limit: 100, starting_after: 2 } })
+        .to_return(api_response(third_body))
+
+      subject.all
+
+      expect(first_stub).to have_been_requested
+      expect(second_stub).to have_been_requested
+      expect(third_stub).to have_been_requested
+    end
+  end
+
+  describe '.first' do
+    subject { self.class::Widget }
+
+    let(:body) { {
+      'object' => 'List',
+      'data' => [ { 'object' => 'Widget', 'vid' => 1, 'id' => 1, 'name' => 'my-widget' } ],
+      'next' => '/widgets?limit=1&starting_after=1',
+      'previous' => '/widgets?limit=1&ending_before=1'
+    }.to_json }
+
+    it 'makes a request with limit 1 then returns the single record' do
+      stub = stub_get('/widgets')
+        .with({ query: { limit: 1 } })
+        .to_return(api_response(body))
+
+      subject.first
+
+      expect(stub).to have_been_requested
     end
   end
 end
