92c482c84cf6f5925943f3d029f2f9bfb861f8e6
Jonathon Storer <jonathon.scott.storer@gmail.com>
2018-10-15 14:26:11 -0400

    wi

--- lib/cashbox/rest/update_subscription.rb
+++ lib/cashbox/rest/update_subscription.rb
@@ -7,32 +7,9 @@ module Cashbox::Rest
     included do
       include Cashbox::Rest::Helpers
 
-      def update_subscription(new_product_id, old_product_id = nil)
-        body = {
-          object: "Subscription",
-          id: self.vid,
-          product: {
-            object: "Product",
-            id: new_product_id
-          }
-        }
-
-        if old_product_id
-          body[:replaces] = {
-            object: "SubscriptionItem",
-            product: {
-              object: "Product",
-              id: old_product_id
-            }
-          }
-        end
-
+      def reset_bill_date
         request = Cashbox::Request.new(:post, "#{route(self.vid)}?effective_date=today&bill_prorated_period=true", {
-          body: {
-            items: [
-              body
-            ]
-          }.to_json
+          body: self.to_json
         })
         self.class.cast(self, request.response)
       end
