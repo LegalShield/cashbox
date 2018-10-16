d736bd323127e1081c6e31ce30e7f6c7444fd385
Hefley <Hefley@users.noreply.github.com>
2018-10-16 14:25:47 -0500

    Feature/update subscription (#71)
    closes #71 
    LGTM: @maulin @jonstorer @douglasrlee @sgelbart

--- lib/cashbox/model/subscription.rb
+++ lib/cashbox/model/subscription.rb
@@ -30,5 +30,29 @@ module Cashbox
     property :policy
     property :starts, coerce: Cashbox::Type.DateTime
     property :status
+
+    def add_subscription_item(product_to_add)
+      subscription_item_to_add = Cashbox::SubscriptionItem.new({
+        product: product_to_add
+      })
+
+      items.push(subscription_item_to_add)
+    end
+
+    def remove_subscription_item(product_to_remove)
+      index = (items || []).index {|subscription_item| subscription_item.product.id == product_to_remove.id }
+      items.delete_at(index) if index
+    end
+
+    def replace_subscription_item(product_to_add, product_to_remove)
+      subscription_item_to_remove = remove_subscription_item(product_to_remove)
+
+      add_subscription_item(product_to_add)
+      items.last.replaces = subscription_item_to_remove.product.id if subscription_item_to_remove
+    end
+
+    def update_subscription_items
+      Cashbox::Request.new(:post, "#{route(id)}?effective_date=today&bill_prorated_period=true", { body: self.to_json })
+    end
   end
 end
