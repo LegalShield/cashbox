670c4f5d974c4263c430817bcf79ebe02f86c782
Jonathon Storer <jonathon.scott.storer@gmail.com>
2018-10-29 12:23:44 -0400

    wip

--- lib/cashbox/request.rb
+++ lib/cashbox/request.rb
@@ -1,8 +1,27 @@
+require 'addressable/uri'
+
+
 module Cashbox
+  class Cache
+    def initialize
+      @cache = {}
+    end
+
+    def set(key, value)
+      @cache[key] = value
+    end
+
+    def get (key)
+      @cache[key]
+    end
+  end
+
   class Request
     include HTTParty
     format :json
     base_uri 'https://api.vindicia.com'
+
+    @@cache = Cashbox::Cache.new
 
     def initialize(method, path, options = {})
       @method  = method
@@ -18,19 +37,55 @@ module Cashbox
       @@after_request_log ||= nil
     end
 
+    def self.before_request_log(&block)
+      @@before_request_log = block if block_given?
+    end
+
+    def self.before_request_log_block
+      @@before_request_log ||= nil
+    end
+
     def response
-      resp = self.class.send(@method, @path, @options.merge(default_options))
-
-      if self.class.after_request_log_block
-        resp.tap do |r|
-          self.class.after_request_log_block.call(@method, @path, @options, r)
-        end
-      end
-
-      Hashie::Mash.new(resp)
+      before_request
+      perform_request
+      after_request
+      parse_response
     end
 
     private
+
+    def before_request
+      if self.class.before_request_log_block
+        self.class.before_request_log_block.call(@method, @path, @options)
+      end
+
+      @response = @@cache.get(cache_key)
+    end
+
+    def perform_request
+      @response ||= self.class.send(@method, @path, @options.merge(default_options))
+    end
+
+    def after_request
+      if self.class.after_request_log_block
+        @response.tap do |res|
+          self.class.after_request_log_block.call(@method, @path, @options, res)
+        end
+      end
+
+      @@cache.set(cache_key, @response)
+    end
+
+    def parse_response
+      Hashie::Mash.new(@response)
+    end
+
+    def cache_key
+      uri = Addressable::URI.new
+      uri.query_values = @options[:query]
+      uri.path = @path
+      uri.to_s
+    end
 
     def default_options
       { basic_auth: { username: Cashbox.username, password: Cashbox.password }, timeout: 100 }
