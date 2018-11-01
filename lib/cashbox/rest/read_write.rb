0693954c965126091b130322ce797905216b6c47
Jonathon Storer <JonathonStorer@legalshieldcorp.com>
2018-11-01 00:31:03 -0400

    Revert "When less than 100 results in index, don't loop for more"
    This reverts commit 3d98127b878b35af11d80053e92638ecda9a6e34.

--- lib/cashbox/rest/read_write.rb
+++ lib/cashbox/rest/read_write.rb
@@ -3,6 +3,7 @@ require 'active_support/concern'
 module Cashbox::Rest
   module ReadWrite
     extend ActiveSupport::Concern
+
     DEFAULT_LIMIT = 100.freeze
 
     included do
@@ -52,7 +53,7 @@ module Cashbox::Rest
         response = Cashbox::Request.new(:get, route, { query: params }).response
         objects = cast(self.new, response)
 
-        if (objects.count == DEFAULT_LIMIT) && (response.next && (max.nil? || objects.count < max))
+        if (response.next && (max.nil? || objects.count < max))
           max -= objects.count if max
           params = Addressable::URI.parse(response.next).query_values
           objects.concat(query(params, max))
