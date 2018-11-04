fe27e84af97bb205c5dbbba536f0a4f0b195e15f
Jonathon Storer <JonathonStorer@legalshieldcorp.com>
2018-11-03 21:26:19 -0400

    Raise error when finding by nil
    updates #84

--- spec/unit/rest/read_write_spec.rb
+++ spec/unit/rest/read_write_spec.rb
@@ -24,6 +24,10 @@ describe Cashbox::Rest::ReadWrite do
       it 'raises argument error when id is missing' do
         expect { subject.find }.to raise_error(ArgumentError)
       end
+
+      it 'raises argument error when id is nil' do
+        expect { subject.find(nil) }.to raise_error(ArgumentError)
+      end
     end
 
     context 'with args' do
