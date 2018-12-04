cc4f4f80d7d091387e43098841778a8d4924788b
Eric Roberts <eric@ercubed.com>
2018-12-04 11:32:07 -0600

    Adding access to subscriptions via the Account and direct access to billing_day

--- lib/cashbox/model/account.rb
+++ lib/cashbox/model/account.rb
@@ -15,5 +15,18 @@ module Cashbox
     property :notify_before_billing
     property :payment_methods, coerce: Cashbox::Type.List(Cashbox::PaymentMethod)
     property :shipping_address, coerce: Cashbox::Address
+
+    # Note: The subscription and billing_day data mapping is based on an assumption of one subscription per account.
+    #       This will need to be updated to handle many-to-many once the UI is updated to display as such.
+
+    def subscription
+      subscription = Subscription.where(account: id)
+    end
+
+    def billing_day
+      return 0 unless subscription.size > 0
+      subscription[0].billing_day
+    end
+
   end
 end
